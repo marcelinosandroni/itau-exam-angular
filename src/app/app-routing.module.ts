1c6bb3955134c7f5ca404fc4360d065abcf7b89a
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-11 18:57:14 -0300

    feature: add app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessEditPageComponent } from './modules/business/pages/business-edit-page/business-edit-page.component';
+import { BusinessHomePageComponent } from './modules/business/pages/business-home-page/business-home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'business',
+    component: BusinessHomePageComponent,
+    pathMatch: 'full',
+  },
+  { path: 'business/:id', component: BusinessEditPageComponent },
+  { path: '', redirectTo: '/business', pathMatch: 'full' },
+  { path: '**', redirectTo: '/business' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
