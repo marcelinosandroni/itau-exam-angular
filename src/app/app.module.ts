6fa3bcc0647a06a439e9862f9b5a4f6c8283579c
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-11 18:57:41 -0300

    refactor: update app dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BusinessModule } from './modules/business/business.module';
+import { CoreModule } from './core/core.module';
+import { FormsModule } from '@angular/forms';
+import { GoBackDirective } from './shared/directive/go-back.directive';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NoopAnimationsModule,
+    CoreModule,
+    BusinessModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
