113c5231cf7e08f3f653a2d5f9fceddaf31acd90
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-12 00:07:58 -0300

    refactor: update mock to math real examples

--- src/app/core/mock/business.mock.ts
+++ src/app/core/mock/business.mock.ts
@@ -3,47 +3,110 @@ import { Business } from "src/app/shared/models/business";
 export const BUSINESS: Business[] = [
   {
     id: 1,
-    name: 'bank name',
-    business: 'business type',
-    valuation: 9,
+    name: 'Itaú BBA',
+    business: 'Centro Financeiro',
+    valuation: 850000000.50,
     active: true,
     cep: '99999-999',
     cnpj: 12345678000001
   },
   {
     id: 2,
-    name: 'bank name',
-    business: 'business type',
-    valuation: 9,
+    name: 'Itáu Ceic',
+    business: 'Centro Empresarial Itaú',
+    valuation: 54000000.45,
     active: true,
     cep: '99999-999',
     cnpj: 12345678000001
   },
   {
     id: 3,
-    name: 'bank name',
-    business: 'business type',
-    valuation: 9,
+    name: 'Cubo Itaú',
+    business: 'Startups Center',
+    valuation: 22000000000.20,
     active: true,
     cep: '99999-999',
     cnpj: 12345678000001
   },
   {
     id: 4,
-    name: 'bank name',
-    business: 'business type',
-    valuation: 9,
+    name: 'Itaú inativo',
+    business: 'Polo Fake',
+    valuation: 0,
+    active: false,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
+  {
+    id: 1,
+    name: 'Itaú BBA',
+    business: 'Centro Financeiro',
+    valuation: 850000000.50,
     active: true,
     cep: '99999-999',
     cnpj: 12345678000001
   },
   {
-    id: 5,
-    name: 'bank name',
-    business: 'business type',
-    valuation: 9,
+    id: 2,
+    name: 'Itáu Ceic',
+    business: 'Centro Empresarial Itaú',
+    valuation: 54000000.45,
     active: true,
     cep: '99999-999',
     cnpj: 12345678000001
-  }
+  },
+  {
+    id: 3,
+    name: 'Cubo Itaú',
+    business: 'Startups Center',
+    valuation: 22000000000.20,
+    active: true,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
+  {
+    id: 4,
+    name: 'Itaú inativo',
+    business: 'Polo Fake',
+    valuation: 0,
+    active: false,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
+  {
+    id: 1,
+    name: 'Itaú BBA',
+    business: 'Centro Financeiro',
+    valuation: 850000000.50,
+    active: true,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
+  {
+    id: 2,
+    name: 'Itáu Ceic',
+    business: 'Centro Empresarial Itaú',
+    valuation: 54000000.45,
+    active: true,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
+  {
+    id: 3,
+    name: 'Cubo Itaú',
+    business: 'Startups Center',
+    valuation: 22000000000.20,
+    active: true,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
+  {
+    id: 4,
+    name: 'Itaú inativo',
+    business: 'Polo Fake',
+    valuation: 0,
+    active: false,
+    cep: '99999-999',
+    cnpj: 12345678000001
+  },
 ]
