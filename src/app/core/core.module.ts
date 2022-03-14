a8ffaa999ea33d283a85fc7f8e2ea5e0dfd77a5b
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-14 13:58:07 -0300

    fix: add new modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,8 +6,22 @@ import { MenuComponent } from './menu/menu.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { getEnvironment } from 'src/environments/environment.provider';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './mock/in-memory-data.service';
+import { SharedModule } from '../shared/shared.module';
 
-
+// Use mocked api if config in development environment is set
+const environment = getEnvironment()
+const devModules = environment.useInMemoryData ? [
+  HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, {
+    dataEncapsulation: false,
+    delay: environment.inMemoryDataDelay
+  }
+  )
+] : []
 
 
 @NgModule({
@@ -18,9 +32,12 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   imports: [
     CommonModule,
+    SharedModule,
+    HttpClientModule,
     MatMenuModule,
     MatDividerModule,
     MatIconModule,
+    ...devModules
   ],
   exports: [
     HeaderComponent,
