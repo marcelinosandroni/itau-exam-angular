3dac810345f814e56fdceacc7f421efc9ff942ba
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-14 13:56:24 -0300

    feat: add pagination component

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,26 @@
+import { AfterViewInit, Component, OnInit, Output, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { LanguageService } from 'src/app/core/language/language.service';
+import { EventEmitter } from '@angular/core';
+import { PaginationIntl } from '../../providers/pagination.provider';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+  providers: [{
+    provide: MatPaginatorIntl, useClass: PaginationIntl
+  }]
+})
+export class PaginationComponent implements AfterViewInit {
+  @Output() setDataSource: EventEmitter<any> = new EventEmitter()
+  @ViewChild(MatPaginator) pagination!: MatPaginator;
+
+  setPagination() {
+    this.setDataSource.emit(this.pagination)
+  }
+
+  ngAfterViewInit(): void {
+    this.setPagination()
+  }
+}
