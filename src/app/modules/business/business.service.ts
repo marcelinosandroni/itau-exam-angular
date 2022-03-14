df44f013944aa405dec180316eb9d0e3c2b0c9e2
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-14 13:58:49 -0300

    feat: add business service

--- src/app/modules/business/business.service.ts
+++ src/app/modules/business/business.service.ts
@@ -1,24 +1,45 @@
-import { Injectable } from '@angular/core';
-import { BUSINESS } from 'src/app/core/mock/business.mock';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoggerService } from 'src/app/core/logger.service';
 import { NOT_FOUND } from 'src/app/shared/constants';
+import { IsLoadingService } from 'src/app/shared/is-loading.service';
 import { Business } from 'src/app/shared/models/business';
+import { Environment } from 'src/environments/environment.interface';
+import { ENVIRONMENT } from 'src/environments/environment.provider';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BusinessService {
   businesses: Business[] = []
+  apiUrl: string
 
-  constructor() {
-    this.businesses = BUSINESS
+  constructor(
+    @Inject(ENVIRONMENT) private environment: Environment,
+    private http: HttpClient,
+    private logger: LoggerService,
+    public isLoadingService: IsLoadingService
+
+  ) {
+    this.logger.context = BusinessService.name
+    this.logger.log(` Starting bussines on ${this.environment.name} environment`)
+    this.logger.log(`API URL: ${this.environment.api}`)
+    this.apiUrl = this.environment.api
   }
 
-  getBusinesses(): Business[] {
-    return this.businesses
+  getBusinesses(): Observable<Business[]> {
+    this.isLoadingService.start()
+    const response$ = this.http.get<Business[]>(this.apiUrl)
+    response$.subscribe(() => {
+      this.isLoadingService.end()
+    })
+
+    return response$
   }
 
-  getBusinessById(id: number): Business | undefined {
-    return this.businesses.find(business => business.id === id)
+  getBusinessById(id: number): Observable<Business> {
+    return this.http.get<Business>(`${this.apiUrl}/${id}`)
   }
 
   getBusinessPosition(id: number): number {
@@ -31,10 +52,11 @@ export class BusinessService {
     return businessIndex
   }
 
-  updateBusiness(id: number, updateBusiness: Partial<Business>): void {
-    const businessPosition = this.getBusinessPosition(id)
-    const foundBusiness = this.businesses[businessPosition]
-    this.businesses[businessPosition] = { ...foundBusiness, ...updateBusiness }
+  updateBusiness(id: number, updateBusiness: Partial<Business>): Observable<Business> {
+    return this.http.put<Business>(`${this.apiUrl}/${id}`, updateBusiness)
+    // const businessPosition = this.getBusinessPosition(id)
+    // const foundBusiness = this.businesses[businessPosition]
+    // this.businesses[businessPosition] = { ...foundBusiness, ...updateBusiness }
   }
 
   removeBusiness(id: number): void {
