fae0b0cb369e0cc50eb2b138f3532516993c2abb
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2022-03-14 13:59:40 -0300

    fix: update modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoBackDirective } from './directive/go-back.directive';
+import { CnpjPipe } from './pipes/cnpj.pipe';
+import { CnpjDirective } from './directive/cnpj.directive';
+import { CepDirective } from './directive/cep.directive';
+import { CepPipe } from './pipes/cep.pipe';
+import { DisableControlDirective } from './directive/disable-control.directive';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http)
+}
 
 @NgModule({
-  declarations: [GoBackDirective],
-  imports: [
-    CommonModule
+  declarations: [GoBackDirective,
+    CnpjPipe,
+    CnpjDirective,
+    CepPipe,
+    CepDirective,
+    DisableControlDirective,
+    PaginationComponent,
+
   ],
-  exports: [GoBackDirective]
+  imports: [
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    MatPaginatorModule
+  ],
+  providers: [CnpjPipe, CepPipe],
+  exports: [
+    GoBackDirective,
+    CnpjPipe,
+    CnpjDirective,
+    CepPipe,
+    CepDirective,
+    DisableControlDirective,
+    TranslateModule,
+    PaginationComponent
+  ]
 })
 export class SharedModule { }
